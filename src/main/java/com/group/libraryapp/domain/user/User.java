d787ed4629a98f31d1990eed5b502429f020d60f
eyeofsol <kingjihong@naver.com>
2023-02-23 16:26:15 +0900

    refactor: User 도메인과 UserLoanHistoryRepository 도메인이 협력하도록 변경
    서비스 단에 있던 로직을 도메인이 수행하도록 리팩토링 했습니다.

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,6 +1,10 @@
 package com.group.libraryapp.domain.user;
 
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User {
@@ -11,6 +15,9 @@ public class User {
     @Column(nullable = false, length = 20) // name = "name"은 생략가능
     private String name;
     private Integer age;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserLoanHistory> userLoanHistories = new ArrayList<>();
 
     protected User() {
 
@@ -37,5 +44,17 @@ public class User {
 
     public void updateName(String name) {
         this.name = name;
+    }
+
+    public void loanBook(String bookName) {
+        this.userLoanHistories.add(new UserLoanHistory(this, bookName));
+    }
+
+    public void returnBook(String bookName) {
+        UserLoanHistory targetHistory = this.userLoanHistories.stream()
+                .filter(history -> history.getBookName().equals(bookName))
+                .findFirst() // optional이 나옴
+                .orElseThrow(IllegalArgumentException::new);
+        targetHistory.doReturn();
     }
 }
